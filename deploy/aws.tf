96bea1254a504207b1316f78d4178da82d3221b2
Manjeet localmac <m.seewooth@live.com>
2023-06-08 18:53:52 +0400

    CD

--- deploy/aws.tf
+++ deploy/aws.tf
@@ -4,7 +4,8 @@ data "aws_ecr_repository" "ten_mse_ecr_repo" {
 
 resource "aws_lambda_function" "ten_mse_function" {
   function_name = "ten-mse"
-  image_uri = ${data.aws}:${var.img_tag}" ## Variable parsed on command line
+  #image_uri = ${data.aws}:${var.img_tag}" ## Variable parsed on command line
+  image_uri = "${data.aws_ecr_repository.ten_mse_ecr_repo.repository_url}:${var.img_tag}"
   package_type = "Image"
   role = aws_iam_role.ten_mse_function_role.arn
 }
